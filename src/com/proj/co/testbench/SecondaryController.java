19667e81b41c0f42b4ebe4ec7d8b2fb646420fd2
Eliza <iulia.martuica@student.upt.ro>
2021-05-20 21:49:22 +0300

    Added a hat

--- src/com/proj/co/testbench/SecondaryController.java
+++ src/com/proj/co/testbench/SecondaryController.java
@@ -4,6 +4,8 @@ import com.proj.co.specs.SystemInformation;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 import java.net.URL;
@@ -20,7 +22,9 @@ public class SecondaryController implements Initializable {
     @FXML
     private Label rndRead;
     @FXML
-    private Label title;
+    private Label titlePartition;
+    @FXML
+    private Label titleSize;
     @FXML
     private Label type;
     @FXML
@@ -29,11 +33,14 @@ public class SecondaryController implements Initializable {
     private Label ram;
     @FXML
     private Label cpu;
+    @FXML
+    private ImageView joben;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //setting the title (selected partition and File Size
-        title.setText("Partition: " + PrimaryController.getInstance().getPartition() + "\nFile Size: " + PrimaryController.getInstance().getSize() + " MB");
+        titlePartition.setText(PrimaryController.getInstance().getPartition());
+        titleSize.setText(PrimaryController.getInstance().getSize() + " MB");
 
         SystemInformation si = new SystemInformation();
 
